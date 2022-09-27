2a2d2e06c1204277c4f5a71535d83a7adffc881a
unknown <adrianjocos@hotmail.com>
2022-09-26 23:41:36 -0500

    Enviar

--- src/test/java/com/nttdata/co/pruebabackend/PruebabackendApplicationTests.java
+++ src/test/java/com/nttdata/co/pruebabackend/PruebabackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.nttdata.co.pruebabackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
-
+import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class PruebabackendApplicationTests {
 
@@ -12,8 +12,8 @@ class PruebabackendApplicationTests {
 		String[] args=new String[1];
 		args[0]=new String("TestMain");
 		try{
-			PruebabackendApplication.main(null);
-			Assert.isTrue( "TestMain".equals(args[0]), "Bad Test Main" );
+			PruebabackendApplication.main(args);
+			assertEquals( "TestMain", args[0], "Bad Test Main" );
 		}catch( Exception ex ){
 			Assert.isTrue( ex instanceof Exception, "Bad Test Main" );
 		}
